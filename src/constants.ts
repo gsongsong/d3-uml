0591586f3e941541d16a7aedb122acf77f795bc7
Seokseong Jeon <sjeon87@gmail.com>
2021-03-10 23:26:45 +0900

    Add relation type constants

---
+++ src/constants.ts
@@ -0,0 +1,7 @@
+export const D3UmlRelAssociation = 'Association';
+export const D3UmlRelDirected = 'DirectedAssociation';
+export const D3UmlRelAggregation = 'Aggregation';
+export const D3UmlRelComposition = 'Composition';
+export const D3UmlRelDependency = 'Dependency';
+export const D3UmlRelGeneralization = 'Generalization';
+export const D3UmlRelInterfaceReal = 'InterfaceRealization';
